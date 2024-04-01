a1c23fc84310a758a371a216f59212715749a332
Shoaib Ashfaq <shoaibashfaq111@gmail.com>
2024-03-31 22:24:21 -0400

    Project Works

---
+++ ios101-project6-tumblr/DetailViewController.swift
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.swift
+//  ios101-project6-tumblr
+//
+//  Created by Shoaib Ashfaq on 3/31/24.
+//
+
+import UIKit
+import Nuke
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var pictureView: UIImageView!
+    
+    @IBOutlet weak var summaryView: UILabel!
+    
+    var post: Post!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Details"
+        summaryView.text = post.caption.trimHTMLTags()
+        
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+            Nuke.loadImage(with: url, into: pictureView)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ios101-project6-tumblr/ViewController.swift
+++ ios101-project6-tumblr/ViewController.swift
@@ -17,6 +17,19 @@ class ViewController: UIViewController, UITableViewDataSource {
 
         tableView.dataSource = self
         fetchPosts()
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let selectedIndexPath = tableView.indexPathForSelectedRow else { return }
+
+        // Get the selected movie from the movies array using the selected index path's row
+        let selectedPost = posts[selectedIndexPath.row]
+
+        // Get access to the detail view controller via the segue's destination. (guard to unwrap the optional)
+        guard let detailViewController = segue.destination as? DetailViewController else { return }
+
+        detailViewController.post = selectedPost
 
     }
 
